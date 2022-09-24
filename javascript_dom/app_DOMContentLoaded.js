0f9dde67b03dfa272c7431ce4ef3f2b62a6b36b1
davidl142 <86446921+davidl142@users.noreply.github.com>
2022-09-24 08:37:18 -0500

    javascript dom precargar antes script
    precargar con html con deferer. DOMContentLoaded en el script

---
+++ javascript_dom/app_DOMContentLoaded.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.getElementById("tituloWeb"));
+  console.log(document.getElementById("tituloWeb").textContent);
+});
+// precargar sin importar donde esta el script en el html y evitamos error.
---
+++ javascript_dom/app_propiedades.js
@@ -0,0 +1,7 @@
+console.log("😍😍😍");
+
+console.log(document);
+console.log(document.head);
+console.log(document.title);
+console.log(document.body);
+console.log(document.domain);
